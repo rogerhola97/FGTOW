6d3fd8cc13953ae51b17e63d01ef4550e3b193fe
Rogelio Ortega <rogerhola97@gmail.com>
2026-09-23 15:03:03 -0600

    ubicacion corregida

--- app/lib/company.ts
+++ app/lib/company.ts
@@ -1,5 +1,5 @@
 export const FABRICATION_ADDRESS = "Miguel Barragán 103, Col. 15 de Mayo, C.P. 64450, Monterrey, N.L.";
-export const FABRICATION_MAPS_URL = `https://www.google.com/maps/search/?api=1&query=${encodeURIComponent(`${FABRICATION_ADDRESS}, México`)}`;
+export const FABRICATION_MAPS_URL = "https://maps.app.goo.gl/ARhdcZuw4ygBqrRx9";
 export const INSTAGRAM_URL = "https://www.instagram.com/fg_tow/";
 export const FACEBOOK_URL = "https://www.facebook.com/profile.php?id=61593682995717";
 export const WHATSAPP_NUMBER = "81 8466 0403";
